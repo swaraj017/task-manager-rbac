d959cb2efa6c94a7a20955b4996e09d371ccbfd4
Swaraj Gaikwad <116364064+swaraj017@users.noreply.github.com>
2026-01-24 17:35:51 +0530

    Change Register form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -77,7 +77,7 @@ export default function Register() {
               onChange={handleChange}
               required
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
-              placeholder="John Doe"
+              placeholder="name"
             />
           </div>
 
