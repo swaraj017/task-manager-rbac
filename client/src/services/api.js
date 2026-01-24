13d867adb56b019263539eb3cbf1ab8d2e436e22
Swaraj Gaikwad <116364064+swaraj017@users.noreply.github.com>
2026-01-24 17:19:40 +0530

    Update URL to production endpoint

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+//"http://localhost:5000/api"
+const API_BASE_URL = "https://task-manager-backend-87v7.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL
